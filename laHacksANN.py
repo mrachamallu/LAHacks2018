b03b67af55b6131b684704ef38123a90c86003c2
Yugantar Prakash <yugantar@s-169-232-111-76.resnet.ucla.edu>
2018-03-31 20:36:23 -0700

    data is being set to matrix correctly

--- laHacksANN.py
+++ laHacksANN.py
@@ -5,44 +5,42 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import classification_report,confusion_matrix
 
-mat_contents = sio.loadmat('ex4data1.mat')
+def ANN(list):
+    
 
-X = mat_contents['X']
-y = mat_contents['y']
+    X = 
+    """
+    import the X dataset
+    import the y dataset
+    """
 
-
-"""
-import the X dataset
-import the y dataset
-"""
-
-#we split the data set into training and testing data
-X_train, X_test, y_train, y_test = train_test_split(X, y)
+    #we split the data set into training and testing data
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
 
 
-"""
-NORMALIZING the dataset
-"""
-scaler = StandardScaler()
-# Fit only to the training data
-scaler.fit(X_train)
+    """
+    NORMALIZING the dataset
+    """
+    scaler = StandardScaler()
+    # Fit only to the training data
+    scaler.fit(X_train)
 
-# Now apply the transformations to the data:
-X_train = scaler.transform(X_train)
-X_test = scaler.transform(X_test)
+    # Now apply the transformations to the data:
+    X_train = scaler.transform(X_train)
+    X_test = scaler.transform(X_test)
 
-# Instance of ANN
-mlp = MLPClassifier(hidden_layer_sizes=(30,30,30))
+    # Instance of ANN
+    mlp = MLPClassifier(hidden_layer_sizes=(30,30,30))
 
-#Training ANN
-mlp.fit(X_train,y_train)
+    #Training ANN
+    mlp.fit(X_train,y_train)
 
-start = time.time()
-#Predicting
-predictions = mlp.predict(X_test)
-end = time.time()
-#
-print("time taken={}".format(end-start) )
-print(confusion_matrix(y_test,predictions))
+    start = time.time()
+    #Predicting
+    predictions = mlp.predict(X_test)
+    end = time.time()
 
-print(classification_report(y_test,predictions))
+    print("time taken={}".format(end-start) )
+    print(confusion_matrix(y_test,predictions))
+
+    print(classification_report(y_test,predictions))
--- sendinfo.py
+++ sendinfo.py
@@ -2,52 +2,63 @@ from liblo import *
 
 import sys 
 import time
+
 import re
+
+matrix = open("data_train.txt", "w+")
+
 
 
 class MuseServer(ServerThread):
     #listen for messages on port 5000
     def __init__(self):
         ServerThread.__init__(self, 5000)
+        self.GL=[]
 
     #receive accelrometer data
     @make_method('/muse/acc', 'fff')
     def acc_callback(self, path, args):
         acc_x, acc_y, acc_z = args
-        print ("%s %f %f %f" % (path, acc_x, acc_y, acc_z))
+        # print ("%s %f %f %f" % (path, acc_x, acc_y, acc_z))
 
     #receive EEG data
     @make_method('/muse/eeg', 'ffff')
     def eeg_callback(self, path, args):
         l_ear, l_forehead, r_forehead, r_ear = args
-        print ("%s %f %f %f %f" % (path, l_ear, l_forehead, r_forehead, r_ear))
+        # print ("%s %f %f %f %f" % (path, l_ear, l_forehead, r_forehead, r_ear))
 
     #handle unexpected messages
     @make_method(None, None)
     def fallback(self, path, args, types, src):
-        print("Unknown message \
+        """ print("Unknown message \
         \n\t Source: '%s' \
         \n\t Address: '%s' \
         \n\t Types: '%s ' \
         \n\t Payload: '%s'" \
-        % (src.url, path, types, args))
+        % (src.url, path, types, args))"""
         word1 = "relative"
         word2 = "absolute"
         word3 = "score"
-        if re.search(word1, path):
-            print("ANN(args)")
-        elif re.search(word2, path):
-            print("word2")
+        if re.search(word1, path) or re.search(word2, path) or re.search(word3, path):
+            # print(args)
+            self.GL = self.GL + args
+            if re.search("theta_session_score", path):
+                matrix.write(str(self.GL))
+                matrix.write("\n")
+
+                self.GL = []
 
 try:
     server = MuseServer()
 except ServerError:
-    print("err")
+    # print("err")
     sys.exit()
 
 
 server.start()
 
 if __name__ == "__main__":
-    while 1:
+    for i in range(100):
         time.sleep(1)
+
+matrix.close()
